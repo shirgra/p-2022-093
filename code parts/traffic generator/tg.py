988c44ee76011c3b019add6d7a98e64ab0212518
Shir Granit <62025130+shirgra@users.noreply.github.com>
2021-11-11 16:20:27 +0200

    add arguments for running the file

--- code parts/traffic generator/tg.py
+++ code parts/traffic generator/tg.py
@@ -17,14 +17,11 @@ def flow_gen(dst_ip, size):
     print("Starting thread with ip: ")
     print(dst_ip)
     flow_dst_ip = dst_ip
-    #dst_ip = "10.0.5.10"
     flow_size = size + "K"
-    #packet_lenght = "0.015625K"
     packet_lenght = "0.015625K"
     bw = "10K"
     # if Packet Lenght is 1 KBytes and BW is 50 Kbits/sec then 
     # BW is 2.5 packets/sec
-
     cmd = "iperf -u -c "
     cmd += dst_ip
     cmd += " -n "
@@ -35,22 +32,22 @@ def flow_gen(dst_ip, size):
     cmd += bw
     os.system(cmd)
 
-def main():
-    total_size = 0
 
+def main(name_csv='flows.csv'):
+    total_size = 0
     count = 0
-    with open('flows.csv') as csvfile:
+    with open(name_csv) as csvfile:
         flows = csv.reader(csvfile, quotechar='|')
         for flow in flows:
             if count > 0:
                 if count > 1:
                     if flow[0] != 'EOF':
-                        thread.start_new_thread(flow_gen,(str(prev_flow[1]),str(prev_flow[2])))
+                        thread.start_new_thread(flow_gen, (str(prev_flow[1]), str(prev_flow[2])))
                         sleep((float(flow[3]) - float(prev_flow[3])))
                         prev_flow = flow
                     else:
                         print(prev_flow)
-                        thread.start_new_thread(flow_gen,(str(prev_flow[1]),str(prev_flow[2])))
+                        thread.start_new_thread(flow_gen, (str(prev_flow[1]), str(prev_flow[2])))
                 else:
                     prev_flow = flow
                     count += 1
@@ -58,10 +55,12 @@ def main():
                 count += 1
     while True:
         sleep(10000)
-
+    # if user pressed ctrl + c
     print("Sender Terminated")
-    
 
 
 if __name__ == '__main__':
-    main()
+    # expects program to run like this:   tg.py flow.csv
+    print("Name of file: "+str(sys.argv))
+    main(str(sys.argv[1]))  # pass as argument the csv file that contains the traffic info
+
